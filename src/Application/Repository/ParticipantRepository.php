810a37d6780ecf1fa828c2e8d83be1709aca7bb6
Borut Balazek <bobalazek124@gmail.com>
2015-09-14 12:01:46 +0200

    Refactoring on statistics + added graphs for participants and entries - by hours

--- src/Application/Repository/ParticipantRepository.php
+++ src/Application/Repository/ParticipantRepository.php
@@ -15,4 +15,44 @@ class ParticipantRepository
             ->getSingleScalarResult()
         ;
     }
+
+    /**
+     * To-Do: This function may need a little refactoring.
+     *
+     * @return array
+     */
+    public function getByHours()
+    {
+        $results = array();
+
+        $databaseResults = $this->createQueryBuilder('p')
+            ->select("p.timeCreated AS date")
+            ->orderBy('p.timeCreated', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+
+        $firstDate = date('Y-m-d H:i:s', strtotime('-2 days'));
+        $lastDate = date('Y-m-d H:i:s');
+        $dates = dateRange($firstDate, $lastDate, '+ 1 hour', 'Y-m-d H');
+
+        foreach ($dates as $date) {
+            $count = 0;
+
+            if ($databaseResults) {
+                foreach ($databaseResults as $databaseResult) {
+                    if (strpos($date, $databaseResult['date']->format('Y-m-d H')) !== false) {
+                        $count++;
+                    }
+                }
+            }
+
+            $results[] = array(
+                'date' => $date.':00',
+                'count' => $count,
+            );
+        }
+
+        return $results;
+    }
 }
