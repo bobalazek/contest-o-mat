33936af39baa8709a11584638828ff63852f6ed9
Borut Balazek <bobalazek124@gmail.com>
2015-09-15 23:44:07 +0200

    Version bump

--- app/configs/global.php
+++ app/configs/global.php
@@ -4,7 +4,7 @@ return array(
     'environment' => 'development',
     'debug' => true,
     'name' => 'Contest-O-Mat',
-    'version' => '0.4',
+    'version' => '0.5',
     'author' => 'Borut Balazek',
 
     // Admin email (& name)
