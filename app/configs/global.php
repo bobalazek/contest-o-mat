5620e6f170a78bc931d70e2bef81ec33069cc64f
Borut Balazek <bobalazek124@gmail.com>
2015-09-17 11:06:58 +0200

    Version bump

--- app/configs/global.php
+++ app/configs/global.php
@@ -4,7 +4,7 @@ return array(
     'environment' => 'development',
     'debug' => true,
     'name' => 'Contest-O-Mat',
-    'version' => '0.8.1',
+    'version' => '0.8.2',
     'author' => 'Borut Balazek',
     // You MUST change the baseUri, in case you'll have multiple instances
     // of this application on the same (sub-)domain.
